23a308288153608103b49bf103713ebe09805810
Matus Faro <matus@matus.io>
2024-11-04 21:47:23 -0500

    Fix nested openApi objects cause deploy issue with ApiGateway

--- dataspray-api-parent/dataspray-api/src/test/java/io/dataspray/api/ApiConstantsTest.java
+++ dataspray-api-parent/dataspray-api/src/test/java/io/dataspray/api/ApiConstantsTest.java
@@ -22,6 +22,7 @@
 
 package io.dataspray.api;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.yaml.snakeyaml.Yaml;
 
@@ -31,15 +32,21 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApiConstantsTest {
 
-    @Test
-    void testTopLevelPaths() throws Exception {
-        Map<String, Object> api;
+    Map<String, Object> api;
+
+    @BeforeEach
+    void beforeEach() throws Exception {
         try (FileReader fileReader = new FileReader("target/generated-sources/api/openapi/openapi.yaml")) {
             api = new Yaml().load(fileReader);
         }
+    }
+
+    @Test
+    void testTopLevelPaths() throws Exception {
         //noinspection unchecked
         Set<String> actual = ((Map<String, Object>) api.get("paths"))
                 .keySet()
@@ -52,5 +59,20 @@ public class ApiConstantsTest {
                 .collect(Collectors.toSet());
 
         assertEquals(Set.copyOf(ApiConstants.TOP_LEVEL_PATHS), actual, "Update TOP_LEVEL_PATHS in ApiConstants");
+    }
+
+    @Test
+    void testModelNames() throws Exception {
+        //noinspection unchecked
+        Map<String, Object> components = (Map<String, Object>) api.get("components");
+        //noinspection unchecked
+        Map<String, Object> schemas = (Map<String, Object>) components.get("schemas");
+        schemas.keySet().forEach(model -> {
+            // AWS ApiGateway requires all Models to be alphanumeric, but OpenAPI generates names on the fly when you
+            // nest an object within an object by separating it with an underscore. This test ensures that all models
+            // are alphanumeric. Otherwise you will get an error during CDK deploy:
+            // > Unable to create model for 'DeployRequest_dynamoState': Model name must be alphanumeric: DeployRequest_dynamoState
+            assertTrue(model.matches("^[a-zA-Z0-9]+$"), "Model names must be alphanumeric, found: '" + model + "'. Likely caused by nested objects, decouple and use $ref.");
+        });
     }
 }
