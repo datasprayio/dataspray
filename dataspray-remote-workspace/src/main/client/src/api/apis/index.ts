261673efb7f6f585f99376a6fbc141c16746ed45
Matus Faro <matus@matus.io>
2026-08-10 15:00:38 +0200

    chore: finish removal of dataspray-remote-workspace and runner .idea dirs
    The previous cleanup commit inadvertently committed untracked generated
    files under the removed module and runner .idea workspace files; remove
    them for good.

--- dataspray-remote-workspace/src/main/client/src/api/apis/index.ts
+++
@@ -1,5 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './AdminApi';
-export * from './FileSystemApi';
-export * from './TerminalApi';
--- dataspray-remote-workspace/src/main/client/src/api/models/index.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './ReadDirectoryResponse';
-export * from './ReadDirectoryResponseFilesInner';
-export * from './StatResponse';
-export * from './WatcherEvent';
